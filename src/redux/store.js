970c5ea32e89b50127a455e28aeae9a07138be24
Martin Mwangi <mwangimartin1904@gmail.com>
2022-11-08 12:19:38 +0300

    Add redux devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import {createStore, combineReducers} from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 import { postReducer } from "./reducers/postReducer"
 
 const rootReducer = combineReducers({
 	postState: postReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeWithDevTools())
